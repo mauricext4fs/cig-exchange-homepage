9e5a87512a452270790ec722e8045e31ac6ef775
alexgblr <alexgblr>
2019-01-25 16:46:22 +0300

    verify otp api call

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	router.HandleFunc(baseURI+"accounts/signup", controllers.CreateAccount).Methods("POST")
 	router.HandleFunc(baseURI+"accounts/signin", controllers.GetAccount).Methods("POST")
 	router.HandleFunc(baseURI+"accounts/send_otp", controllers.SendCode).Methods("POST")
+	router.HandleFunc(baseURI+"accounts/verify_otp", controllers.VerifyCode).Methods("POST")
 	router.HandleFunc(baseURI+"contact_us", controllers.SendContactUsEmail).Methods("POST")
 
 	//attach JWT auth middleware
